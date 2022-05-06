4cd9882a125fe508f4510660db9291d80d337b5c
Jasper van Merle <jaspervmerle@gmail.com>
2022-05-06 23:01:12 +0200

    Add link to Kaggle notebook

--- src/pages/home/LoadFromKaggle.tsx
+++ src/pages/home/LoadFromKaggle.tsx
@@ -4,11 +4,10 @@ import { HomeCard } from './HomeCard';
 export function LoadFromKaggle(): JSX.Element {
   return (
     <HomeCard title="Load from Kaggle">
-      {/* TODO: Add link */}
       {/* prettier-ignore */}
       <Text>
         Episodes can be loaded straight from Kaggle notebooks.
-        See the <a href="#" target="_blank" rel="noreferrer">Koreye 2022 integration</a> notebook for instructions.
+        See the <a href="https://www.kaggle.com/code/jmerle/koreye-2022-integration" target="_blank" rel="noreferrer">Koreye 2022 integration</a> notebook for instructions.
       </Text>
     </HomeCard>
   );
